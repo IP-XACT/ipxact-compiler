1a111c3f5f3913fb5c85c97e7d0c96f6140b6b04
mtravaillard <maxim.travaillard@gmail.com>
2026-09-02 15:01:47 +0200

    Change version handling logic

--- src/ipxact/__init__.py
+++ src/ipxact/__init__.py
@@ -1,13 +1,7 @@
-from importlib.metadata import PackageNotFoundError
-from importlib.metadata import version as _version
+__version__ = "0.1.0"
 
 from .parser import parse_element, parse_file
 from .schema import *  # noqa: F401,F403
 from .schema import __all__ as _schema_all
-
-try:
-    __version__ = _version("ipxact-compiler")
-except PackageNotFoundError:
-    __version__ = "0.0.0+unknown"
 
 __all__ = [*_schema_all, "parse_element", "parse_file", "__version__"]
